362bdc6b26ff7f3ffc151c5770303a0d6d76dba6
MoreThanOneAnimal <morethanoneanimal@gmail.com>
2015-01-07 12:42:26 +0100

    ignoring isMoving field

--- app/model/Torpedo.java
+++ app/model/Torpedo.java
@@ -1,14 +1,18 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Torpedo extends MovingObject {
 
 	private final double range = 50.0d;
 	private double distance = 0.0d;
 
+	public Torpedo() { super(0, 0, 0, 0);}
 	public Torpedo(double x, double y, double deltaX, double deltaY) {
 		super(x, y, deltaX, deltaY);
 	}
 
+	@JsonIgnore
 	public boolean isMoving() {
 		return distance < range;
 	}
